1fc93cbd0837e59e94a58b74942284c9c387fc8d
pkdash <pkdash_reena@hotmail.com>
2016-09-15 16:27:14 -0600

    Changes to python scripts to work with ArcGIS Pro (Python 3)

--- pyfiles/ArcGISParameterRegionTool.py
+++ pyfiles/ArcGISParameterRegionTool.py
@@ -1,4 +1,4 @@
-__author__ = 'Pabitra'
+# created by: Pabitra Dash
 
 import arcpy
 import os
@@ -61,5 +61,7 @@ arcpy.AddMessage('\nProcess started:\n')
 start_message = "Please wait. It may take few seconds. Computation is in progress ..."
 arcpy.AddMessage(start_message)
 for line in process.stdout.readlines():
+    if isinstance(line, bytes):	    # true in Python 3
+        line = line.decode()
     if start_message not in line:
         arcpy.AddMessage(line)
--- pyfiles/DinfConcenLimitAccum.py
+++ pyfiles/DinfConcenLimitAccum.py
@@ -11,71 +11,74 @@ import subprocess
 # Inputs
 inlyr = arcpy.GetParameterAsText(0)
 desc = arcpy.Describe(inlyr)
-ang=str(desc.catalogPath)
-arcpy.AddMessage("\nInput D-Infinity Flow Direction Grid: "+ang)
+ang = str(desc.catalogPath)
+arcpy.AddMessage("\nInput D-Infinity Flow Direction Grid: " + ang)
 
 inlyr1 = arcpy.GetParameterAsText(1)
 desc = arcpy.Describe(inlyr1)
-wg=str(desc.catalogPath)
-arcpy.AddMessage("Input Effective Runoff Weight Grid: "+wg)
+wg = str(desc.catalogPath)
+arcpy.AddMessage("Input Effective Runoff Weight Grid: " + wg)
 
 inlyr2 = arcpy.GetParameterAsText(2)
 desc = arcpy.Describe(inlyr2)
-dg=str(desc.catalogPath)
-arcpy.AddMessage("Input Disturbance Indicator Grid: "+dg)
+dg = str(desc.catalogPath)
+arcpy.AddMessage("Input Disturbance Indicator Grid: " + dg)
 
 inlyr3 = arcpy.GetParameterAsText(3)
 desc = arcpy.Describe(inlyr3)
-dm=str(desc.catalogPath)
-arcpy.AddMessage("Input Decay Multiplier Grid: "+dm)
+dm = str(desc.catalogPath)
+arcpy.AddMessage("Input Decay Multiplier Grid: " + dm)
 
 ogrfile=arcpy.GetParameterAsText(4)
 if arcpy.Exists(ogrfile):
     desc = arcpy.Describe(ogrfile)
-    shfl1=str(desc.catalogPath)
-    extn=os.path.splitext(shfl1)[1] # get extension of a file
- # if extention is shapfile do not convert into gjson other wise convert
-    if extn==".shp":
-       shfl=shfl1;
+    shfl1 = str(desc.catalogPath)
+    extn = os.path.splitext(shfl1)[1]   # get extension of a file
+    # if extention is shapfile do not convert into gjson other wise convert
+    if extn == ".shp":
+       shfl = shfl1
     else:
       arcpy.AddMessage("Extracting json outlet file from: "+shfl1)
-      basename = os.path.basename(shfl1) # get last part of the path
-      dirname=os.path.dirname(ang) # get directory
-      arcpy.env.workspace = dirname # does not work without specifying the workspace
-      arcpy.FeaturesToJSON_conversion(shfl1,basename+".json") # convert feature to json
-      shfl=os.path.join(dirname,basename+".json")
-    arcpy.AddMessage("Using Outlets file: "+shfl)
+      basename = os.path.basename(shfl1)   # get last part of the path
+      dirname = os.path.dirname(ang)    # get directory
+      arcpy.env.workspace = dirname     # does not work without specifying the workspace
+      arcpy.FeaturesToJSON_conversion(shfl1, basename + ".json")   # convert feature to json
+      shfl = os.path.join(dirname, basename + ".json")
+    arcpy.AddMessage("Using Outlets file: " + shfl)
 
-concthresh=arcpy.GetParameterAsText(5)
-arcpy.AddMessage("Concentration Threshold: "+concthresh)
+concthresh = arcpy.GetParameterAsText(5)
+arcpy.AddMessage("Concentration Threshold: " + concthresh)
 
-edgecontamination=arcpy.GetParameterAsText(6)
-arcpy.AddMessage("Edge Contamination: "+edgecontamination)
+edgecontamination = arcpy.GetParameterAsText(6)
+arcpy.AddMessage("Edge Contamination: " + edgecontamination)
 
 # Input Number of Processes
-inputProc=arcpy.GetParameterAsText(7)
-arcpy.AddMessage("Number of Processes: "+inputProc)
+inputProc = arcpy.GetParameterAsText(7)
+arcpy.AddMessage("Number of Processes: " + inputProc)
 
 # Output
 q = arcpy.GetParameterAsText(8)
-arcpy.AddMessage("Output Overland Flow Specific Discharge Grid: "+q)
+arcpy.AddMessage("Output Overland Flow Specific Discharge Grid: " + q)
 
 ctpt = arcpy.GetParameterAsText(9)
-arcpy.AddMessage("Output Concentration Grid: "+ctpt)
+arcpy.AddMessage("Output Concentration Grid: " + ctpt)
 
 # Construct command 1
-cmd = 'mpiexec -n ' + inputProc + ' AreaDinf -ang ' + '"' + ang + '"' + ' -sca ' + '"' + q + '"' + ' -wg ' + '"' + wg + '"'
-arcpy.AddMessage("\nCommand Line: "+cmd)
+cmd = 'mpiexec -n ' + inputProc + ' AreaDinf -ang ' + '"' + ang + '"' + ' -sca ' + '"' + q + '"' + \
+      ' -wg ' + '"' + wg + '"'
+arcpy.AddMessage("\nCommand Line: " + cmd)
 
 # Submit command to operating system
 os.system(cmd)
 
 # Capture the contents of shell command and print it to the arcgis dialog box
-process=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-#arcpy.AddMessage('\nProcess started:\n')
-message="\n"
+process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+message = "\n"
 for line in process.stdout.readlines():
-    message=message+line
+    if isinstance(line, bytes):	    # true in Python 3
+        line = line.decode()
+    message = message+line
 arcpy.AddMessage(message)
 
 # Calculate statistics on the output so that it displays properly
@@ -83,23 +86,27 @@ arcpy.AddMessage('Calculate Statistics\n')
 arcpy.CalculateStatistics_management(q)
 
 # Construct command 2
-cmd = 'mpiexec -n ' + inputProc + ' DinfConcLimAccum -ang ' + '"' + ang + '"' + ' -dg ' + '"' + dg + '"' + ' -dm ' + '"' + dm + '"' + ' -ctpt ' + '"' + ctpt + '"' + ' -q ' + '"' + q + '"' + ' -csol ' + concthresh
+cmd = 'mpiexec -n ' + inputProc + ' DinfConcLimAccum -ang ' + '"' + ang + '"' + ' -dg ' + '"' + dg + \
+      '"' + ' -dm ' + '"' + dm + '"' + ' -ctpt ' + '"' + ctpt + '"' + ' -q ' + '"' + q + '"' + \
+      ' -csol ' + concthresh
 if arcpy.Exists(ogrfile):
     cmd = cmd + ' -o ' + '"' + shfl + '"'
 if edgecontamination == 'false':
     cmd = cmd + ' -nc '
 
-arcpy.AddMessage("\nCommand Line: "+cmd)
+arcpy.AddMessage("\nCommand Line: " + cmd)
 
 # Submit command to operating system
 os.system(cmd)
 
 # Capture the contents of shell command and print it to the arcgis dialog box
-process=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
-#arcpy.AddMessage('\nProcess started:\n')
-message="\n"
+process = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE)
+
+message = "\n"
 for line in process.stdout.readlines():
-    message=message+line
+    if isinstance(line, bytes):	    # true in Python 3
+        line = line.decode()
+    message = message + line
 arcpy.AddMessage(message)
 
 # Calculate statistics on the output so that it displays properly
@@ -107,9 +114,9 @@ arcpy.AddMessage('Calculate Statistics\n')
 arcpy.CalculateStatistics_management(ctpt)
 # remove converted json file
 if arcpy.Exists(ogrfile):
-  extn_json=os.path.splitext(shfl)[1] # get extension of the converted json file
-  if extn_json==".json":
-    os.remove(shfl)
+    extn_json = os.path.splitext(shfl)[1]    # get extension of the converted json file
+    if extn_json == ".json":
+        os.remove(shfl)
 
 
 
