5420a03047fdb12135b71559de6284bc948e56c0
dtarb <dtarb@usu.edu>
2016-11-23 21:21:44 -0700

    Add minimum area capability to catchment outlets

--- src/CatchOutlets.h
+++ src/CatchOutlets.h
@@ -2,5 +2,5 @@
 
 
 //int catchoutlets(char *pfile, char *streamnetsrc, char *streamnetlyr, char *outletsdatasrc, char *outletslayer, int  lyrno, float maxdist);
-int catchoutlets(char *pfile, char *streamnetsrc,char *outletsdatasrc, double mindist, int gwstartno);
+int catchoutlets(char *pfile, char *streamnetsrc,char *outletsdatasrc, double mindist, int gwstartno, double minarea);
 //  Incomplete implementation no capability for layers within data sources yet
