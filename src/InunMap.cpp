a5fa8c8c329ef4b8660b99915bf34fd63a0ffb18
pkdash <pabitra.dash@usu.edu>
2025-06-03 13:33:23 -0400

    using gdal instead of netcdf for inun map

--- src/InunMap.cpp
+++ src/InunMap.cpp
@@ -47,7 +47,8 @@ email:  dtarb@usu.edu
 #include "linearpart.h"
 #include "createpart.h"
 #include "tiffIO.h"
-#include <netcdf.h> // using C API; C++ API is not as updaed
+#include <gdal.h>
+#include <gdal_priv.h>
 
 int inunmap(char *handfile, char*catchfile, char *maskfile, char*fcfile, int maskpits, char*hpfile, char *mapfile)
 {
@@ -107,39 +108,37 @@ int inunmap(char *handfile, char*catchfile, char *maskfile, char*fcfile, int mas
 		long nfc; // num of COMIDs with forecast
         long *catchlist; double *hlist;
 		if (rank == 0) {
-			int ncid, varidCatch, varidH;
-			if (nc_open(fcfile, NC_NOWRITE, &ncid)) {
-				fprintf(stderr, "NetCDF: error open file %s\n", fcfile); exit(1);
-			}
-			size_t n;
-			if (nc_inq_dimlen(ncid, 0, &n)) {
-				fprintf(stderr, "NetCDF: error get dim size \n"); exit(1);
-			}
-			nfc = (long) n;
-			char t[512];
-			if (nc_get_att_text(ncid, NC_GLOBAL, "Timestamp", t)) {
-				fprintf(stderr, "NetCDF: error get timestamp attr \n"); exit(1);
-			}
+			GDALAllRegister();
+            GDALDataset *poDS = (GDALDataset *) GDALOpen(fcfile, GA_ReadOnly);
+            if (poDS == NULL) {
+                fprintf(stderr, "GDAL: error opening file %s\n", fcfile);
+                exit(1);
+            }
 
-			catchlist = (long *) malloc(sizeof(long) * nfc);
-			hlist = (double *) malloc(sizeof(double) * nfc);
-			
-			if (nc_inq_varid(ncid, "COMID", &varidCatch) || nc_inq_varid(ncid, "H", &varidH)) {
-				fprintf(stderr, "NetCDF: error get varid for comid and H\n"); exit(1);
-			}
-			if (nc_get_var_long(ncid, varidCatch, catchlist)) {
-				fprintf(stderr, "NetCDF: error read comid list\n"); exit(1);
-			}
-			if (nc_get_var_double(ncid, varidH, hlist)) {
-				fprintf(stderr, "NetCDF: error read h list\n"); exit(1);
-			}
-			printf("Forecast input: %s\n", fcfile);
-			printf("\tnum_COMIDs: %ld\n", nfc);
-			printf("\tTimestamp: %s\n", t);
-			int l = (nfc>5)?5:nfc;
-			printf("COMID:\t");for (int i=0; i<l; i++) printf("%ld ", catchlist[i]);printf("\n");
-			printf("H:\t");for (int i=0; i<l; i++) printf("%lf ", hlist[i]);printf("\n");
-			nc_close(ncid);
+            // Read COMID and H variables
+            GDALRasterBand *poBandCOMID = poDS->GetRasterBand(1); // Adjust band number as needed
+            GDALRasterBand *poBandH = poDS->GetRasterBand(2);     // Adjust band number as needed
+
+            nfc = poBandCOMID->GetXSize(); // Assuming 1D array
+            catchlist = (long *) malloc(sizeof(long) * nfc);
+            hlist = (double *) malloc(sizeof(double) * nfc);
+
+            // Read data
+            if (poBandCOMID->RasterIO(GF_Read, 0, 0, nfc, 1, catchlist, nfc, 1, GDT_Int32, 0, 0) != CE_None ||
+                poBandH->RasterIO(GF_Read, 0, 0, nfc, 1, hlist, nfc, 1, GDT_Float64, 0, 0) != CE_None) {
+                fprintf(stderr, "GDAL: error reading data\n");
+                exit(1);
+            }
+
+            // Get timestamp from metadata
+            char **metadata = poDS->GetMetadata();
+            printf("Forecast input: %s\n", fcfile);
+            printf("\tnum_COMIDs: %ld\n", nfc);
+            if (metadata != NULL) {
+                printf("\tTimestamp: %s\n", CSLFetchNameValue(metadata, "TIMESTAMP"));
+            }
+
+            GDALClose(poDS);
 		}
 		MPI_Bcast(&nfc, 1, MPI_LONG, 0, MCW);
 		if (rank != 0) {
@@ -158,91 +157,66 @@ int inunmap(char *handfile, char*catchfile, char *maskfile, char*fcfile, int mas
 		unordered_map<int, float> inunratiohash;
 		if (maskpits) {
 			if (rank == 0) {
-				int ncid, varidCatch, varidH, varidA, varidS;
-				if (nc_open(hpfile, NC_NOWRITE, &ncid)) {
-					fprintf(stderr, "hpNetCDF: error open file %s\n", hpfile); exit(1);
-				}
-				size_t n;
-				if (nc_inq_dimlen(ncid, 0, &n)) {
-					fprintf(stderr, "hpNetCDF: error get dim size \n"); exit(1);
-				}
-				if (nc_inq_varid(ncid, "CatchId", &varidCatch) || nc_inq_varid(ncid, "Stage", &varidH) || nc_inq_varid(ncid, "SurfaceArea", &varidS) || nc_inq_varid(ncid, "AREASQKM", &varidA)) {
-					fprintf(stderr, "hpNetCDF: error get varid for comid and H\n"); exit(1);
-				}
-				long nrows = (long) n;
-				int nstages = 83; //TODO: get the val from netcdf itself
-				nhp = nrows / nstages; // must be divisible by nstages
-				// read variables into memory: only stage=1ft
-				hydrocatchlist = (long *) malloc(sizeof(long) * nhp); // output
-				inunratiolist = (float*) malloc(sizeof(float) * nhp); // output
-				long *hcatchlist = (long *) malloc(sizeof(long) * nhp); 
-				double *surfacearealist = (double*) malloc(sizeof(double) * nhp);
-				double *areasqkmlist = (double*) malloc(sizeof(double) * nhp);
-				size_t nhp2 = nhp;
-				size_t index = 1; // 1ft is the 2nd stage on stage table. TODO: get it from a config
-				ptrdiff_t interval = 83; //TODO: get it from a config
-				if (nc_get_vars_long(ncid, varidCatch, &index, &nhp2, &interval, hcatchlist)) {
-					fprintf(stderr, "hpNetCDF: error read comid list\n"); exit(1);
-				}
-				if (nc_get_vars_double(ncid, varidS, &index, &nhp2, &interval, surfacearealist)) {
-					fprintf(stderr, "hpNetCDF: error read surfacearea list\n"); exit(1);
-				}
-				if (nc_get_vars_double(ncid, varidA, &index, &nhp2, &interval, areasqkmlist)) {
-					fprintf(stderr, "hpNetCDF: error read areasqkm list\n"); exit(1);
+				GDALDataset *poDS = (GDALDataset *) GDALOpen(hpfile, GA_ReadOnly);
+				if (poDS == NULL) {
+					fprintf(stderr, "GDAL: error opening hydroprop file %s\n", hpfile);
+					exit(1);
 				}
 
-				for (int i=0; i<nhp; i++) {
-					float inunratio = (float)(surfacearealist[i] / (areasqkmlist[i] * 1000000.0));
-					if (inunratio < 0.1) continue; // if ratio > 10%, we should check if to show it or not
-					hydrocatchlist[hpcount] = hcatchlist[i];	
-					inunratiolist[hpcount] = inunratio;	
-					hpcount ++;
-				}
-				int ll = (nhp>5)?5:nhp;
-				printf("Check: input hydrotable\n");
-				printf("COMID:\t");for (int i=0; i<ll; i++) printf("%ld\t", hcatchlist[i]);printf("\n");
-				printf("S:\t");for (int i=0; i<ll; i++) printf("%.5f\t", surfacearealist[i]);printf("\n");
-				printf("A:\t");for (int i=0; i<ll; i++) printf("%.5f\t", areasqkmlist[i]);printf("\n");
-				free(hcatchlist); free(surfacearealist); free(areasqkmlist);
-/* DEPRECATED: the following reads nc row by row, very slow. 
-				long comid; double areqsqkm, surfacearea, h;
-			
-				if (nc_inq_varid(ncid, "CatchId", &varidCatch) || nc_inq_varid(ncid, "Stage", &varidH) || nc_inq_varid(ncid, "SurfaceArea", &varidS) || nc_inq_varid(ncid, "AREASQKM", &varidA)) {
-					fprintf(stderr, "hpNetCDF: error get varid for comid and H\n"); exit(1);
-				}
+				// Read variables using GDAL bands
+				// Adjust band numbers based on your NetCDF structure
+				GDALRasterBand *poBandCatchId = poDS->GetRasterBand(1);
+				GDALRasterBand *poBandStage = poDS->GetRasterBand(2);
+				GDALRasterBand *poBandSurfArea = poDS->GetRasterBand(3);
+				GDALRasterBand *poBandAreaSqKm = poDS->GetRasterBand(4);
+
+				// Get the number of records
+				nhp = poBandCatchId->GetXSize(); // Assuming 1D array
+				
+				// Allocate memory for temporary arrays
+				long *catchids = (long *) malloc(sizeof(long) * nhp);
+				float *stages = (float *) malloc(sizeof(float) * nhp);
+				float *surfareas = (float *) malloc(sizeof(float) * nhp);
+				float *areasqkms = (float *) malloc(sizeof(float) * nhp);
+
+				// Read data from bands
+				if (poBandCatchId->RasterIO(GF_Read, 0, 0, nhp, 1, catchids, nhp, 1, GDT_Int32, 0, 0) != CE_None ||
+                    poBandStage->RasterIO(GF_Read, 0, 0, nhp, 1, stages, nhp, 1, GDT_Float32, 0, 0) != CE_None ||
+                    poBandSurfArea->RasterIO(GF_Read, 0, 0, nhp, 1, surfareas, nhp, 1, GDT_Float32, 0, 0) != CE_None ||
+                    poBandAreaSqKm->RasterIO(GF_Read, 0, 0, nhp, 1, areasqkms, nhp, 1, GDT_Float32, 0, 0) != CE_None) {
+                    fprintf(stderr, "GDAL: error reading hydroprop data\n");
+                    exit(1);
+                }
+				// Allocate arrays for comid and inunratio
 				hydrocatchlist = (long *) malloc(sizeof(long) * nhp);
-				memset(hydrocatchlist, 0, nhp *sizeof(long));
-				inunratiolist = (float*) malloc(sizeof(float) * nhp);
-				memset(inunratiolist, 0, nhp *sizeof(float));
-				for (size_t i=0; i<nrows; i++) { // scan hydroprop table to find areasqkm at Stage=1ft
-					if (nc_get_var1_double(ncid, varidH, &i, &h)) {
-						fprintf(stderr, "hpNetCDF: error read Stage\n"); exit(1);
-					}
-					if (h<0.3 || h>0.4) continue; //TODO: the interval needs to be computed
-					// stage=1ft row
-					if (nc_get_var1_double(ncid, varidS, &i, &surfacearea)) {
-						fprintf(stderr, "hpNetCDF: error read SurfaceArea\n"); exit(1);
-					}
-					if (nc_get_var1_double(ncid, varidA, &i, &areqsqkm)) {
-						fprintf(stderr, "hpNetCDF: error read AREASQKM\n"); exit(1);
-					}
-					float inunratio = (float)(surfacearea / (areqsqkm * 1000000.0));
-					if (inunratio < 0.1) continue; // if ratio > 10%, we should check if to show it or not
-					if (nc_get_var1_long(ncid, varidCatch, &i, &comid)) {
-						fprintf(stderr, "hpNetCDF: error read comid\n"); exit(1);
-					}
-					hydrocatchlist[hpcount] = comid;
-					inunratiolist[hpcount] = inunratio;
-					hpcount ++;
-				}
-*/
-				printf("Hydroproperty input: %s\n", hpfile);
-				printf("\tnum_COMIDs: %ld\n", nhp);
-				printf("\tnum_COMIDs to mask: %ld\n", hpcount);
-				int l = (hpcount>5)?5:hpcount;
-				printf("COMID:\t");for (int i=0; i<l; i++) printf("%ld ", hydrocatchlist[i]);printf("\n");
-				printf("RATIO:\t");for (int i=0; i<l; i++) printf("%.5f ", inunratiolist[i]);printf("\n");
-				nc_close(ncid);
+				inunratiolist = (float *) malloc(sizeof(float) * nhp);
+
+                // Process the data to calculate inundation ratios
+                for (int idx = 0; idx < nhp; idx++) {
+                    if (areasqkms[idx] > 0) {
+                        float inunratio = surfareas[idx] / areasqkms[idx];
+                        if (inunratio > 0.1) { // 10% threshold
+                            hydrocatchlist[hpcount] = catchids[idx];
+                            inunratiolist[hpcount] = inunratio;
+                            hpcount++;
+                        }
+                    }
+                }
+
+                // Reallocate arrays to actual size needed
+                hydrocatchlist = (long *) realloc(hydrocatchlist, sizeof(long) * hpcount);
+                inunratiolist = (float *) realloc(inunratiolist, sizeof(float) * hpcount);
+
+                // Clean up temporary arrays
+                free(catchids);
+                free(stages);
+                free(surfareas);
+                free(areasqkms);
+
+                printf("Hydroprop input: %s\n", hpfile);
+                printf("\tnum_COMIDs processed: %ld\n", hpcount);
+
+                GDALClose(poDS);
 			}
 			MPI_Bcast(&hpcount, 1, MPI_LONG, 0, MCW);
 			if (rank != 0) {
