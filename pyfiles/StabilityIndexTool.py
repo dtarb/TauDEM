113af1db1c2fa93591b71524ff88d2e70363b704
pkdash <pabitra.dash@usu.edu>
2026-06-04 15:08:46 -0400

    feat: streamline StabilityIndexTool to use sinmapsi executable instead of stabilityindex execuatble

--- pyfiles/StabilityIndexTool.py
+++ pyfiles/StabilityIndexTool.py
@@ -1,7 +1,3 @@
-# Created by: Pabitra Dash
-
-import os
-
 import arcpy
 
 import Utils
@@ -10,59 +6,56 @@ import Utils
 slp_raster_file = arcpy.GetParameterAsText(0)
 desc = arcpy.Describe(slp_raster_file)
 slp_raster_file = str(desc.catalogPath)
+arcpy.AddMessage("\nInput Slope raster file: " + slp_raster_file)
 
 sca_raster_file = arcpy.GetParameterAsText(1)
 desc = arcpy.Describe(sca_raster_file)
 sca_raster_file = str(desc.catalogPath)
+arcpy.AddMessage("Input SCA raster file: " + sca_raster_file)
 
 cal_raster_file = arcpy.GetParameterAsText(2)
 desc = arcpy.Describe(cal_raster_file)
 cal_raster_file = str(desc.catalogPath)
+arcpy.AddMessage("Input CAL raster file: " + cal_raster_file)
 
 capl_text_file = arcpy.GetParameterAsText(3)
+arcpy.AddMessage("Input CAPL text file: " + capl_text_file)
+
 min_terr_recharge = arcpy.GetParameterAsText(4)
+arcpy.AddMessage("Input Minimum Terrain Recharge: " + min_terr_recharge)
+
 max_terr_recharge = arcpy.GetParameterAsText(5)
+arcpy.AddMessage("Input Maximum Terrain Recharge: " + max_terr_recharge)
+
 si_raster_file = arcpy.GetParameterAsText(6)
+arcpy.AddMessage("Output Stability Index raster file: " + si_raster_file)
+
 sat_raster_file = arcpy.GetParameterAsText(7)
+arcpy.AddMessage("Output Saturation raster file: " + sat_raster_file)
+
 temp_output_files_directory = arcpy.GetParameterAsText(8)
 is_delete_intermediate_output_files = arcpy.GetParameterAsText(9)
 
-# create the cis_inputs.txt file from the provided above parameters
-
-
-si_control_file = os.path.join(temp_output_files_directory, 'Si_Control.txt')
-
-with open(si_control_file, 'w') as file_obj:
-    file_obj.write('# input parameters for combined stability index computation with road impact\n')
-    file_obj.write('# input files\n')
-    file_obj.write('slp=' + slp_raster_file + '\n')
-    file_obj.write('sca=' + sca_raster_file + '\n')
-    file_obj.write('cal=' + cal_raster_file + '\n')
-    file_obj.write('calpar=' + capl_text_file + '\n')
-
-    file_obj.write('# output files\n')
-    file_obj.write('si=' + si_raster_file + '\n')
-    file_obj.write('sat=' + sat_raster_file + '\n')
-
-    file_obj.write('# Additional parameters' + '\n')
-    file_obj.write('minimumterrainrecharge=' + min_terr_recharge + '\n')
-    file_obj.write('maximumterrainrecharge=' + max_terr_recharge + '\n')
-    file_obj.write('# temporary output file directory\n')
-    file_obj.write('temporary_output_files_directory=' + temp_output_files_directory + '\n')
-    if str(is_delete_intermediate_output_files) == 'true':
-        file_obj.write('is_delete_intermediate_output_files=True\n')
-    else:
-        file_obj.write('is_delete_intermediate_output_files=False\n')
-
+input_proc = arcpy.GetParameterAsText(10)
+arcpy.AddMessage('Number of Processes: ' + input_proc)
+# Note: temp_output_files_directory and is_delete_intermediate_output_files are retained
+# as toolbox parameters for backward compatibility, but are not used when calling
+# sinmapsi directly (no intermediate files are generated in this wrapper).
+arcpy.AddMessage('\nStarting Stability Index computation...')
 try:
-    arcpy.AddMessage('\nStarting Stability Index computation...')
-
-    cmd = 'stabilityindex --params ' + Utils.quote(si_control_file)
+    cmd = 'mpiexec -n ' + input_proc + ' SinmapSI' + \
+          ' -slp ' + Utils.quote(slp_raster_file) + \
+          ' -sca ' + Utils.quote(sca_raster_file) + \
+          ' -calpar ' + Utils.quote(capl_text_file) + \
+          ' -cal ' + Utils.quote(cal_raster_file) + \
+          ' -si ' + Utils.quote(si_raster_file) + \
+          ' -sat ' + Utils.quote(sat_raster_file) + \
+          ' -par ' + str(min_terr_recharge) + ' ' + str(max_terr_recharge) + ' 9.81 1000'
     arcpy.AddMessage('\nCommand Line: ' + cmd)
 
     return_code = Utils.run_taudem_command(cmd, arcpy.AddMessage)
     if return_code != 0:
-        raise Exception('stabilityindex failed with return code ' + str(return_code))
+        raise Exception('SinmapSI failed with return code ' + str(return_code))
 
     arcpy.AddMessage('\nStability Index computation completed successfully.')
 
